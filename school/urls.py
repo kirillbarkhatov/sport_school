b595156f002453e54040739e82052b79aa98fe8f
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-11-10 19:27:54 +0300

    Подтянут шаблон

---
+++ school/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from school.apps import SchoolConfig
+
+from . import views
+
+app_name = SchoolConfig.name
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index")
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
