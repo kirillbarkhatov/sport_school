9507d5281cb53e4865f96653a1232c12834bdb84
kirill_barkhatov <k.s.barkhatov@gmail.com>
2025-10-17 21:25:25 +0300

    Revert "Улучшения шаблонов2"
    This reverts commit a8a1e7f86bc7398bfbe1c4f2d840cb1f5880cf03.

--- members/views.py
+++ members/views.py
@@ -48,7 +48,6 @@ class PersonListView(ApprovedUserRequiredMixin, ListView):
         if can_manage:
             context["families"] = list(Family.objects.order_by("family_name"))
             context["relation_choices"] = FamilyMember.FAMILY_RELATION
-            context["person_create_form"] = PersonForm()
         return context
 
 
@@ -131,6 +130,9 @@ class FamilyListView(ApprovedUserRequiredMixin, ListView):
         )
         if self.request.user.is_staff or self.request.user.is_superuser:
             self._attach_candidate_people(families)
+            context["all_families"] = families
+            context["relation_choices"] = FamilyMember.FAMILY_RELATION
+            context["person_form"] = PersonForm()
         return context
 
     def post(self, request, *args, **kwargs):
