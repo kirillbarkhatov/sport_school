09d74dfa2109756c73863725da50507814298c5a
kirill_barkhatov <k.s.barkhatov@gmail.com>
2025-10-23 15:46:13 +0300

    дополнены чойсы и миграции

--- school/choices.py
+++ school/choices.py
@@ -24,6 +24,8 @@ class TrainingLocation(models.TextChoices):
     SNEZHNY = ("snezny", "Снежный")
     SERVERNY_SLOPE = ("serverny_slope", "Серверный склон")
     OKHTA_PARK = ("okhta_park", "Охта парк")
+    PARK_HOUSE = ("park_house", "Бугры (ТЦ Парк хаус)")
+    SEVER_PARK = ("sever_park", "Север парк арена (Руставели 38)")
     OTHER = ("other", "Локация уточняется")
 
 
@@ -31,6 +33,7 @@ class TrainingEquipment(models.TextChoices):
     ATHLETIC = ("athletic", "Спортивная одежда, кроссовки")
     ROLLERS = ("rollers", "Ролики, защита")
     SKATES = ("skates", "Коньки, защита")
+    ICE = ("ice", "Коньки")
     BIKE = ("bike", "Велосипед, защита")
     SLALOM_SKI = ("slalom_ski", "Слаломные лыжи, защита")
     GS_SKI = ("gs_ski", "Лыжи для гигантского слалома, защита")
